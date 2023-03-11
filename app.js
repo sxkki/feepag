88d3c38c79cae9e1063998a2dc8acf6e898059b8
sxkki <lmmr8393@gmail.com>
2023-03-10 20:32:05 -0400

    modificacion total del fee web

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ function calcula(){
   var monto = parseInt(document.fvalida.monto.value);
   var metodo = document.fvalida.metodo.value;
   var tarifas = {
-      "metodo1": 0.155,
-      "metodo2": 0.085,
-      "metodo3": 0.085
+      "metodo1": 0.07
   };
   var tarifa = tarifas[metodo] * monto;
   var total = monto + tarifa;
 
   document.getElementById('fee').innerHTML = tarifa;
   document.getElementById('total').innerHTML = total;
+}
+
+function calculaV1(){
+  if (document.fvalida.monto.value === "" || document.fvalida.metodo.selectedIndex==0) {
+    alert('Por favor ingrese una cantidad y seleccione un metodo.');
+    document.fvalida.monto.focus();
+    return;
+}
+var monto = parseInt(document.fvalida.monto.value);
+var metodo = parseInt(document.fvalida.metodo.value);
+var tarifas = {
+    "metodo1": 0.07
+};
+var tarifa = tarifas[metodo] * monto;
+var total = monto + tarifa;
+
+document.getElementById('fee').innerHTML = tarifa;
+document.getElementById('total').innerHTML = total;
 }
