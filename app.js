9f751bbc1245fc8c8499c32b53bc8e4bf270a867
sekki <lmmr8393@icloud.com>
2024-09-12 20:49:34 -0400

    update 2.0
    updating view and more

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-function calcula(){
-  if (document.fvalida.monto.value === "" || document.fvalida.metodo.selectedIndex==0) {
-      alert('Por favor ingrese una cantidad y seleccione un metodo.');
-      document.fvalida.monto.focus();
-      return;
-  }
-  var monto = parseInt(document.fvalida.monto.value);
-  var metodo = document.fvalida.metodo.value;
-  var tarifas = {
-      "metodo1": 7
-  };
-  var tarifa = tarifas[metodo] * monto / 100;
-  var total = monto + tarifa;
+function calcularV(){
+  if (document.fvalida.monto.value === "" || document.fvalida.metodo.value === "") {
+    alert('Por favor ingrese una cantidad y seleccione un metodo.');
+    document.fvalida.monto.focus();
+    return;
+}
+var monto = parseFloat(document.fvalida.monto.value);
+var metodo = parseFloat(document.fvalida.metodo.value);
+var tarifa = metodo * monto / 100;
+var total = monto + tarifa;
 
-  document.getElementById('fee').innerHTML = tarifa;
-  document.getElementById('total').innerHTML = total;
+document.getElementById('fee').innerHTML = tarifa.toFixed(2);
+document.getElementById('total').innerHTML = total.toFixed(2);
 }
 
 function calculaV1(){
@@ -27,6 +24,23 @@ var metodo = parseFloat(document.fvalida.metodo.value);
 var tarifa = metodo * monto / 100;
 var total = monto + tarifa;
 
-document.getElementById('fee').innerHTML = tarifa.toFixed(2);
-document.getElementById('total').innerHTML = total.toFixed(2);
+document.getElementById('fee').value = tarifa.toFixed(2);
+document.getElementById('total').value = total.toFixed(2);
+}
+
+function calculaV2(){
+  if (document.Pvalida.montoRecib.value === "") {
+    alert('Por favor ingrese una cantidad y seleccione un metodo.');
+    document.fvalida.monto.focus();
+    return;
+}
+var fee1 = parseFloat(document.Pvalida.fee1.value);
+var fee2 = parseFloat(document.Pvalida.fee2.value);
+var montoR = parseFloat(document.Pvalida.montoRecib.value);
+var resultComi = montoR * fee1 / 100;
+var resultFee = resultComi + fee2;
+var resultEnvi = montoR + resultFee;
+
+document.getElementById('montoEnvio').value = resultEnvi.toFixed(2);
+document.getElementById('montoFee').value = resultFee.toFixed(2);
 }
